7d6fbd4476738e16d8ad0f3bfa7e3c32e1cc9ab9
mrspartan38 <mrspartan38@gmail.com>
2026-01-15 12:08:20 -0600

    critical mass of code

---
+++ package.mjs
@@ -0,0 +1,25 @@
+{
+  "name": "package",
+  "version": "1.0.0",
+  "type": "module",
+  "description": "",
+  "keywords": [],
+  "homepage": "https://github.com/mrspartan38/fanta-devops#readme",
+  "bugs": {
+    "url": "https://github.com/mrspartan38/fanta-devops/issues"
+  },
+  "repository": {
+    "type": "git",
+    "url": "git+https://github.com/mrspartan38/fanta-devops.git"
+  },
+  "license": "ISC",
+  "author": "",
+  "type": "commonjs",
+  "main": "index.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "dependencies": {
+    "express": "^5.2.1"
+  }
+}
