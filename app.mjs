6b071fa940873c5125df5e0756e6f2f21251b217
Evan S. <mrspartan38@gmail.com>
2026-01-28 18:23:28 -0600

    Update app.mjs

--- app.mjs
+++ app.mjs
@@ -1,3 +1,5 @@
+//app.js
+//es5 syntax => import express from 'express'
 import express from 'express'
 
 const app = express()
@@ -6,6 +8,7 @@ app.get('/', (req, res) => {
   res.send('Hello Express')
 })
 
+//start the server. 
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000')
 })
